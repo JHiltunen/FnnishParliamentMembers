9534d6e088e993f2cb0110c75d2120e7c17150b6
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-03-01 10:39:56 +0200

    Created ParliamentMember entity

--- app/src/main/java/com/jhiltunen/finnishparliamentmembers/logic/models/ParliamentMember.kt
+++ app/src/main/java/com/jhiltunen/finnishparliamentmembers/logic/models/ParliamentMember.kt
@@ -1,11 +1,30 @@
 package com.jhiltunen.finnishparliamentmembers.logic.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "parliament_members")
 data class ParliamentMember(
+        @PrimaryKey(autoGenerate = false)
+        @ColumnInfo(name = "heteka_id")
         val hetekaId: Int,
+
+        @ColumnInfo(name = "seat_number")
         val seatNumber: Int = 0,
+
+        @ColumnInfo(name = "lastname")
         val lastname: String,
+
+        @ColumnInfo(name = "firstname")
         val firstname: String,
+
+        @ColumnInfo(name = "party")
         val party: String,
+
+        @ColumnInfo(name = "minister")
         val minister: Boolean = false,
+
+        @ColumnInfo(name = "picture_url")
         val pictureUrl: String = ""
 )
