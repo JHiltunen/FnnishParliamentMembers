805ee885cf9c9a3b8d0b3512a2ec29316b9b0b34
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-03-05 10:16:37 +0200

    Removed unused code

--- app/src/main/java/com/jhiltunen/finnishparliamentmembers/logic/viewmodels/ParliamentMemberViewModel.kt
+++ app/src/main/java/com/jhiltunen/finnishparliamentmembers/logic/viewmodels/ParliamentMemberViewModel.kt
@@ -17,51 +17,5 @@ class ParliamentMemberViewModel(application: Application): AndroidViewModel(appl
         private val parliamentDao: ParliamentDao = ParliamentDatabase.getInstance(application).parliamentDao()
     private val parliamentRepository: ParliamentRepository = ParliamentRepository(parliamentDao)
 
-    /*init {
-        Log.d("***", "INIT, setup recurringwork")
-        setupRecurringWork()
-    }*/
 
-    /*fun insertMemberToDatabase(member: ParliamentMember) {
-        //Log.d("INSERT" , parliamentMembers.value!![0].firstname)
-        viewModelScope.launch { parliamentRepository.insertParliamentMember(member) }
-    }*/
-
-    /*val fetchRequest =
-        PeriodicWorkRequestBuilder<FetchApiWork>(1, TimeUnit.HOURS)
-            // Additional configuration
-            .build()
-*/
-    private fun setupRecurringWork() {
-        Log.d("***", "settingUpRecurringWork...")
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .setRequiresCharging(false)
-            .setRequiresBatteryNotLow(false)
-            .apply {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    setRequiresDeviceIdle(true)
-                }
-            }
-            .build()
-
-        val repeatingRequest = PeriodicWorkRequestBuilder<FetchApiWork>(15, TimeUnit.MINUTES)
-            .setConstraints(constraints)
-            .build()
-
-        WorkManager.getInstance(this.getApplication()).enqueueUniquePeriodicWork(
-            FetchApiWork.WORK_FETCH,
-            ExistingPeriodicWorkPolicy.KEEP,
-            repeatingRequest)
-    }
-
-    fun refreshParliamentMemberInfo() {
-        viewModelScope.launch {
-            try {
-                parliamentRepository.fetchParliamentInfoFromApi()
-            } catch (e: IOException) {
-                Log.d("ERROR", e.toString())
-            }
-        }
-    }
 }
